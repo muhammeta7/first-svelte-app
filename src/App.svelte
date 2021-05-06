58fb94d9ba44a50c92d490c93ae77a1ed6aa1a16
muhammeta7 <muhammeta7@gmail.com>
2021-05-05 21:19:35 -0400

    create User component and send props to parent App component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+    import User from "./User.svelte"
     import { onMount } from 'svelte'
     let users;
 
@@ -9,12 +10,8 @@
     function getGitHubUsers() {
         fetch("https://api.github.com/users")
         .then(resp => resp.json())
-        .then(data => {
-            console.log(data);
-            users = data;
-        });
+        .then(data => (users = data));
     }
-
 </script>
 
 <style>
@@ -22,10 +19,13 @@
         list-style: none;
         margin: 0;
         padding: 0;
+        display: flex;
+        flex-flow: wrap;
     }
 
-    img{
-        max-width: 50px;
+    li {
+        width: 20%;
+        margin-bottom: 20px;
     }
 </style>
 
@@ -33,70 +33,11 @@
 <main>
     {#if users}
         <ul>
-    {#each users as user}
-        <li>
-            <img src={user.avatar_url} alt={`${user.login}'s Avatar`}>
-            {user.login}
-        </li>
-    {/each}
+        {#each users as user}
+            <li>
+                <User username={user.login} avatar="{user.avatar_url}" />
+            </li>
+        {/each}
         </ul>
-        {/if}
+    {/if}
 </main>
-<!--<header class="header">-->
-<!--    <h1 class="header-title">Svelte powered markdown editor</h1>-->
-<!--</header>-->
-
-<!--<div class="markdown-editor">-->
-<!--    <div class="markdown-editor__left-panel">-->
-<!--        <textarea bind:value="{source}" class="markdown-editor__source"></textarea>-->
-<!--    </div>-->
-
-<!--    <div class="markdown-editor__right-panel">-->
-<!--        <div class="markdown-editor__output">{@html markdown}</div>-->
-<!--    </div>-->
-<!--</div>-->
-
-<!--<style>-->
-<!--    .header {-->
-<!--        height: 10vh;-->
-<!--        display: flex;-->
-<!--        align-items: center;-->
-<!--        justify-content: center;-->
-<!--    }-->
-
-<!--    .header-title {-->
-<!--        margin: 0;-->
-<!--    }-->
-
-<!--    .markdown-editor {-->
-<!--        width: 100%;-->
-<!--        display: flex;-->
-<!--        align-items:flex-start;-->
-<!--        justify-content: space-evenly;-->
-<!--    }-->
-
-<!--    .markdown-editor__left-panel, .markdown-editor__right-panel {-->
-<!--        width: 50%;-->
-<!--        border: solid 1px black;-->
-<!--        height: 90vh;-->
-<!--    }-->
-
-<!--    .markdown-editor__right-panel {-->
-<!--        overflow: auto;-->
-<!--    }-->
-
-<!--    .markdown-editor__source {-->
-<!--        border: none;-->
-<!--        width: 100%;-->
-<!--        height: 100%;-->
-<!--    }-->
-
-<!--    .markdown-editor__source:focus {-->
-<!--        outline: none;-->
-<!--    }-->
-
-<!--    .markdown-editor__output {-->
-<!--        width: 100%;-->
-<!--        padding: 0 2em;-->
-<!--    }-->
-<!--</style>-->
---
+++ src/User.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let username;
+    export let avatar;
+</script>
+
+<style>
+    img {
+        max-width: 50px;
+    }
+
+    h3 {
+        margin: 0;
+    }
+</style>
+
+<img src={avatar} alt={`${username}'s Avatar`}>
+<h3>{username}</h3>
